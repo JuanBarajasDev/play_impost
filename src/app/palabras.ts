b72fe3b19f1314f10de6b2a6d3a4898b806f44a0
Cami-Hot <familiabarajasg2020@gmail.com>
2026-02-11 23:38:32 -0500

    añadir categorias y su lista de palabras

--- src/app/palabras.ts
+++ src/app/palabras.ts
@@ -1,7 +1,19 @@
 export const PALABRAS_CODIFICADAS: string[] = [
   'Y2FzYQ==',
- 'cGFycXVl',  
+ 'cGFycXVl',
   'ZXNjdWVsYQ==',
   'aG9zcGl0YWw=',
   'YWVyb3B1ZXJ0bw=='
 ];
+
+export const NUEVAS_PALABRAS: {tipo: string, palabras: Array<string>}[] = [
+  {tipo: 'Objetos Cotidianos', palabras: ['Y2FzYQ==','bWVzYQ==','c2lsbGE=','cHVlcnRh','dmVudGFuYQ==','Y2VsdWxhcg==','bGlicm8=','Y3VhZGVybm8=','Y29tcHV0YWRvcg==','cmVsb2o=','emFwYXRv','Y2FtaXNh','cGFudGFsb24=','bW9jaGlsYQ==','Ym90ZWxsYQ==','cGxhdG8=','Y3VjaGFyYQ==','dGVsZXZpc29y','Y2FtYQ==','bGFtcGFyYQ=='  ]},
+  {tipo: 'Comidas y Bebidas', palabras: ['Y2FmZQ==','YXJyb3o=','bGVjaGU=','cGFu','YWd1YQ==','c29wYQ==','ZW1wYW5hZGE=','YXJlcGE=','YmFuZGVqYQ==','YWd1YXBhbmVsYQ==','anVnYQ==','Y2VydmV6YQ==','dGFjbw==','cGl6emE=','ZW5zYWxhZGE=','Y2Fybg==','cGVzY2Fkbw==','cGFzdGE=','aGVsYWRv','Y2hvY29sYXRl']},
+  {tipo: 'Animales', palabras: ['cGVycm8=','Z2F0bw==','Y2FiYWxsbw==','dmFjYQ==','dGlncmU=','bGVvbg==','bW9ubw==','cGV6','cGFqYXJv','b3ZlamE=','Y2VyZG8=','ZWxlZmFudGU=','cmF0b24=','Z2FsbGluYQ==','ZGFyZG8=','YmFsbGVuYQ==','ZG9sZmlu','bG9ibw==','cmVubw==','aGFtYWNv']},
+  {tipo: 'Paises', palabras: ['Y29sb21iaWE=','bWV4aWNv','YXJnZW50aW5h','YnJhc2ls','Y2hpbGU=','cGVydQ==','Y2FuYWRh','ZnJhbmNpYQ==','aXRhbGlh','ZXNwYW5h','YWxlbWFuaWE=','amFwb24=','Y2hpbmE=','aW5kaWE=','ZWdpcHRv','cG9ydHVnYWw=','c3VpemE=','cGFuYW1h','dXJ1Z3VheQ==','Y29yZWE=']},
+  {tipo: 'Colores', palabras: ['cm9qbw==','YXp1bA==','dmVyZGU=','bmVncm8=','YmxhbmNv','YW1hcmlsbG8=','bW9yYWRv','bmFyYW5qYQ==','Z3Jpcw==','cm9zYWRv','dHVycXVlc2E=','bWFycm9u','YmVpZ2U=','dmlvbGV0YQ==','ZG9yYWRv','cGxhdGE=','aW5kaWdv','Y2lhbg==','bGl0YQ==','Y2VsZXN0ZQ==']},
+  {tipo: 'Pasatiempos y Actividades', palabras: ['bGVlcg==','YmFpbGFy','Y2FudGFy','ZGlidWphcg==','Y29ycmVy','bmFkYXI=','Y29jaW5hcg==','dmlhamFy','cGludGFy','anVnYXI=','ZXN0dWRpYXI=','cHJvZ3JhbWFy','Y2ljbGlzbW8=','Zm90b2dyYWZpYQ==','ZXNjcmliaXI=','Y2FtcGFy','c2FsaXI=','ZXNjYWxhcg==','bWVkaXRhcg==','bmF2ZWdhcg==']},
+  {tipo: 'Deportes', palabras: ['ZnV0Ym9s','dGVuaXM=','YmFsb25jZXN0bw==','dm9sZWlib2w=','Ym94ZW8=','bmF0YWNpb24=','Y2ljbGlzbW8=','YXRsZXRpc21v','YmVpc2JvbA==','cnVnYnk=','Z29sZg==','YWplZHJleg==','cGF0aW5hamU=','c2VyZmluZw==','ZXNxdWk=','aGFuZGJhbGw=','dGFla3dvbmRv','aG9ja2V5','Y3JpY2tldA==','YnJpZGdl']},
+  {tipo: 'Clima y Naturaleza', palabras: ['bGx1dmlh','dmllbnRv','dG9ybWVudGE=','bmlldmU=','c29s','Y2xpbWE=','cmlv','bWFy','bW9udGFuYQ==','Ym9zcXVl','ZGVzaWVydG8=','c2VsdmE=','aXNsYQ==','dm9sY2Fu','bnViZQ==','dHJ1ZW5v','cmVsYW1wYWdv','cGxheWE=','bGFnZw==','dmFsbGU=']},
+
+]
